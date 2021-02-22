7fb69834b2ab318110d6e5c2e55f591d3a2fa2e6
Pedro Henrique <67549131+pedrohd21@users.noreply.github.com>
2021-02-22 15:29:47 -0300

    Atualizando

---
+++ conteudo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ConteudoConfig(AppConfig):
+    name = 'conteudo'
---
+++ conteudo/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Conteudo(models.Model):
+    STATUS = (
+        ('doing', 'Doing'),
+        ('done', 'Done'),
+    )
+    titulo = models.CharField(max_length=255)
+    descricao = models.TextField()
+    done = models.CharField(
+        max_length=5,
+        choices=STATUS,
+    )
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_update = models.DateTimeField(auto_now=True)
+
+    
+    def __str__(self):
+        return self.titulo
